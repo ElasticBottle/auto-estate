81c7744c08b3b77d816f432b1900ad2aa9856f7e
Lanke09 <32267117+Lanke09@users.noreply.github.com>
2022-06-03 16:11:40 -0400

    add ROI values

--- app/routes/calculator/property-investment/report/roi-analysis.tsx
+++ app/routes/calculator/property-investment/report/roi-analysis.tsx
@@ -33,83 +33,83 @@ export default function RoiAnalysis() {
           <tbody>
             <Tr>
               <Th isSticky>Gross ROI (Capital Gains)</Th>
-              <Td>Blue</Td>
-              <Td>Blue</Td>
-              <Td>Blue</Td>
-              <Td>Blue</Td>
-              <Td>Blue</Td>
+              <Td>44.93%</Td>
+              <Td>68%</Td>
+              <Td>32.22%</Td>
+              <Td>16%</Td>
+              <Td>0%</Td>
             </Tr>
             <Tr>
               <Th isSticky>Annual Income</Th>
-              <Td>Purple</Td>
-              <Td>Purple</Td>
-              <Td>Purple</Td>
-              <Td>Purple</Td>
-              <Td>Purple</Td>
+              <Td>38400</Td>
+              <Td>7500</Td>
+              <Td>12500</Td>
+              <Td>0</Td>
+              <Td>3000</Td>
             </Tr>
             <Tr>
               <Th isSticky>First Time Home Capital Gains over term</Th>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
+              <Td>44930</Td>
+              <Td>64690</Td>
+              <Td>32200</Td>
+              <Td>16000</Td>
+              <Td>0</Td>
             </Tr>
             <Tr>
               <Th isSticky>EBITA over term</Th>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
+              <Td>236930</Td>
+              <Td>102190</Td>
+              <Td>94700</Td>
+              <Td>16000</Td>
+              <Td>15000</Td>
             </Tr>
             <Tr>
               <Th isSticky>Tax Obligation</Th>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
+              <Td>94772</Td>
+              <Td>30657</Td>
+              <Td>28410</Td>
+              <Td>0</Td>
+              <Td>0</Td>
             </Tr>
             <Tr>
               <Th isSticky>Tax Deductions Available</Th>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
+              <Td>60000</Td>
+              <Td>0</Td>
+              <Td>0</Td>
+              <Td>0</Td>
+              <Td>0</Td>
             </Tr>
             <Tr>
               <Th isSticky>NET Gains</Th>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
+              <Td>202158</Td>
+              <Td>71533</Td>
+              <Td>66290</Td>
+              <Td>16000</Td>
+              <Td>15000</Td>
             </Tr>
             <Tr>
               <Th isSticky>NET Rate of Return over term</Th>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
+              <Td>202%</Td>
+              <Td>71.5%</Td>
+              <Td>66.29%</Td>
+              <Td>16%</Td>
+              <Td>15%</Td>
             </Tr>
             <Tr>
               <Th isSticky>Inflation Rate over term</Th>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
+              <Td>16%</Td>
+              <Td>16%</Td>
+              <Td>16%</Td>
+              <Td>16%</Td>
+              <Td>16%</Td>
             </Tr>
             <Tr>
               <Th isSticky>Real Rate of Return</Th>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
-              <Td>Red</Td>
+              <Td>186%</Td>
+              <Td>55.5%</Td>
+              <Td>50.29%</Td>
+              <Td>0%</Td>
+              <Td>-1%</Td>
             </Tr>
           </tbody>
         </table>
